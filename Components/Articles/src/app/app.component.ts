f901d7b375eed046e6535459cf6e451e850520a3
Qsen02 <diyangenchev73@gmail.com>
2024-11-01 14:10:47 +0200

    article component update

--- Components/Articles/src/app/app.component.ts
+++ Components/Articles/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TitleComponent } from './title/title.component';
 import { ArticleComponent } from './article/article.component';
+import { ArticleItemComponent } from './article/articleItem/articleItem.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TitleComponent,ArticleComponent],
+  imports: [RouterOutlet,TitleComponent,ArticleComponent,ArticleItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
