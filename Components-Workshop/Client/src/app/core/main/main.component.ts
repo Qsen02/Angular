681fa68ff8bb6be6b3ac55d050d9a4dc5d6f814d
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-11-08 19:08:14 +0200

    Main implemented

---
+++ Components-Workshop/Client/src/app/core/main/main.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { PostListComponent } from './post-list/post-list.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [PostListComponent,ThemeListComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+}
