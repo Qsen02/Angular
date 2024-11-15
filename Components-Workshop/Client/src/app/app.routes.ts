1d92a47cf4b5cdcceead131dff827b27b54fcb76
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-11-15 11:30:06 +0200

    Created add theme component

--- Components-Workshop/Client/src/app/app.routes.ts
+++ Components-Workshop/Client/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { ThemeListComponent } from './theme-list/theme-list.component';
+import { AddThemeComponent } from './theme-list/add-theme/add-theme.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
@@ -11,6 +12,7 @@ export const routes: Routes = [
     { path: "login", component: LoginComponent },
     { path: "register", component: RegisterComponent },
     { path: "themes", component: ThemeListComponent },
+    { path: "add-theme", component: AddThemeComponent },
     { path: "404", component: NotFoundComponent },
     { path: "**", redirectTo: "/404", pathMatch: "full" }
 ];
