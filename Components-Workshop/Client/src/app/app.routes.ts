b51f32fd111e85c65f13ad167b563e6891434ac3
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-11-15 10:25:29 +0200

    Created login and register form

--- Components-Workshop/Client/src/app/app.routes.ts
+++ Components-Workshop/Client/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
     { path: "home", component: HomeComponent },
+    { path: "login", component: LoginComponent },
+    { path: "register", component: RegisterComponent },
     { path: "404", component: NotFoundComponent },
     { path: "**", redirectTo: "/404", pathMatch: "full" }
 ];
