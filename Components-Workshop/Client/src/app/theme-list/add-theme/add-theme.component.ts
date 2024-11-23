6428d3d76443ff2b72f20fee6427e24044cae083
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-11-23 10:47:06 +0200

    Implemented validation of the new theme form

--- Components-Workshop/Client/src/app/theme-list/add-theme/add-theme.component.ts
+++ Components-Workshop/Client/src/app/theme-list/add-theme/add-theme.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-add-theme',
-  standalone: true,
-  imports: [],
-  templateUrl: './add-theme.component.html',
-  styleUrl: './add-theme.component.css'
+    selector: 'app-add-theme',
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: './add-theme.component.html',
+    styleUrl: './add-theme.component.css'
 })
 export class AddThemeComponent {
+    constructor(private router: Router) { }
+    onSubmit(form: NgForm) {
+        if (form.invalid) {
+            return;
+        }
+        this.router.navigate(["/themes"]);
+    }
 
+    onCancel() {
+        this.router.navigate(["/home"]);
+    }
 }
